141c19fb4d6a965e9fa6881c67e7b5a583eda1de
Roman Zhikharev <rromms@gmail.com>
2021-06-01 09:31:21 +0300

    09-pingpong v2

--- 09-pingpong/pingpong.go
+++ 09-pingpong/pingpong.go
@@ -3,64 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"sync"
 	"time"
 )
 
-var response map[string]string
-
-const MatchPoint = 15
-
 func main() {
 	ch := make(chan string)
 	wg := sync.WaitGroup{}
-	response = make(map[string]string)
-	response["begin"] = "ping"
-	response["ping"] = "pong"
-	response["PING"] = "stop"
-	response["pong"] = "ping"
-	response["PONG"] = "stop"
-	response["stop"] = "begin"
-
-	wg.Add(2)
-	go player("Player 1", ch, &wg)
-	go player("Player 2", ch, &wg)
-	ch <- "begin"
-	wg.Wait()
-	fmt.Printf("%s  -  %s", <-ch, <-ch)
+	res := make(map[string]int)
+	p1 := "Player 1"
+	p2 := "Player 2"
+	for res[p1] < 10 && res[p2] < 10 {
+		wg.Add(2)
+		go player(p1, ch, &wg)
+		go player(p2, ch, &wg)
+		ch <- "begin"
+		wg.Wait()
+		res[<-ch]++
+		fmt.Printf("%s (%v) - %s (%v)\n", p1, res[p1], p2, res[p2])
+	}
 }
 
 func player(name string, ch chan string, wg *sync.WaitGroup) {
-	scr := 0
 	for {
 		msg := <-ch
-		if msg == "I win" {
-			wg.Done()
-			ch <- fmt.Sprintf("%s (%v)", name, scr)
-			break
-		}
 		if msg == "stop" {
-			scr++
-			if scr == MatchPoint {
-				ch <- "I win"
-				wg.Done()
-				ch <- fmt.Sprintf("%s (%v)", name, scr)
-				break
-			}
+			wg.Done()
+			ch <- name
+			return
 		}
-		msg = randomize(response[msg])
-		fmt.Printf("%s: %s\n", name, msg)
+		rand.Seed(time.Now().UnixNano())
+		if msg != "begin" && rand.Intn(100) < 20 {
+			ch <- "stop"
+			wg.Done()
+			return
+		}
+		if msg == "ping" {
+			msg = "pong"
+		} else {
+			msg = "ping"
+		}
+		fmt.Printf("%s: %v\n", name, msg)
+		time.Sleep(time.Millisecond * 100)
 		ch <- msg
 	}
-}
-
-func randomize(msg string) string {
-	if msg == "ping" || msg == "pong" {
-		rand.Seed(time.Now().UnixNano())
-		if (rand.Intn(100)) < 20 {
-			msg = strings.ToUpper(msg)
-		}
-	}
-	return msg
 }
